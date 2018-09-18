fa68c0f9fa00dd6b810edae75ea3f375eb7b3271
Ian Lai <ian.explore.world@gmail.com>
2018-09-18 16:38:16 +0900

    WebApp: Add template

--- WebApp/server.go
+++ WebApp/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -27,11 +28,34 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s", r.URL.Path[1:])
 }
 
+// func viewHandler(w http.ResponseWriter, r *http.Request) {
+// 	title := r.URL.Path[len("/view/"):]
+// 	log.Println("viewHandler: title " + title)
+// 	p, _ := loadPage(title)
+// 	fmt.Fprintf(w, "<h1>%s</h1> <div>%s</div>", p.Title, p.Body)
+// }
+
+// func editHandler(w http.ResponseWriter, r *http.Request) {
+// 	title := r.URL.Path[len("/edit/"):]
+// 	p, err := loadPage(title)
+// 	if err != nil {
+// 		p = &Page{Title: title}
+// 	}
+// 	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+// 		"<form action=\"/save/%s\" method=\"POST\">"+
+// 		"<textarea name=\"body\">%s</textarea><br>"+
+// 		"<input type=\"submit\" value=\"Save\">"+
+// 		"</form>",
+// 		p.Title, p.Title, p.Body)
+// }
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	log.Println("viewHandler: title " + title)
 	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1> <div>%s</div>", p.Title, p.Body)
+	renderTemplate(w, "view", p)
+	// t, _ := template.ParseFiles("view.html")
+	// t.Execute(w, p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
@@ -40,12 +64,14 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
-		"<form action=\"/save/%s\" method=\"POST\">"+
-		"<textarea name=\"body\">%s</textarea><br>"+
-		"<input type=\"submit\" value=\"Save\">"+
-		"</form>",
-		p.Title, p.Title, p.Body)
+	renderTemplate(w, "edit", p)
+	// t, _ := template.ParseFiles("edit.html")
+	// t.Execute(w, p)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
 }
 
 func main() {
